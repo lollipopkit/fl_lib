5ac5cb1ed929f43e6f80d136ac95721dc5059f9a
GT610 <myddz1005@163.com>
2026-03-28 10:45:16 +0800

    refactor (string extension): Simplify the parameters of the getFileName method
    Remove the unused separator parameter and retain only the withoutExtension parameter

--- lib/src/core/ext/string.dart
+++ lib/src/core/ext/string.dart
@@ -140,11 +140,8 @@ extension StringPathX on String {
 
   /// Get the file name from the path.
   ///
-  /// - [separator] is the separator of the path.
   /// - [withoutExtension] is whether to remove the extension
-  ///
-  /// {@macro string_path_separator}
-  String? getFileName({String? separator, bool withoutExtension = false}) {
+  String? getFileName({bool withoutExtension = false}) {
     final index = lastIndexOf(Pfs.seperator);
     if (index == -1) return null;
     final wholeName = substring(index + 1);
