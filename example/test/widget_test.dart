fbe271e0367d3ef2eea206475b5c554fe7152acb
GT610 <79314033+GT-610@users.noreply.github.com>
2026-04-18 10:37:07 +0800

    Fix analyzer issues and align example test (#32)
    * Fix analyze issues and example test
    
    * Allow example pubspec lockfile
    
    * Stabilize example widget test scrolling

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -11,20 +11,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:example/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('example home page renders', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    final introPageTile = find.widgetWithText(ListTile, 'Intro Page');
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('fl_lib demo'), findsOneWidget);
+    expect(find.byType(ListTile), findsWidgets);
+    await tester.scrollUntilVisible(introPageTile, 20);
+    expect(introPageTile, findsOneWidget);
+    expect(find.byIcon(Icons.chevron_right), findsWidgets);
   });
 }
